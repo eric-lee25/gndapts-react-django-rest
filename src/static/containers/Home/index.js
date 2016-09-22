69009a9dab174a3b2d9377182850ea467f2c55bb
Manish Sinha <arithmetic@gmail.com>
2016-09-22 16:16:21 -0700

    splash page

--- src/static/containers/Home/index.js
+++ src/static/containers/Home/index.js
@@ -21,36 +21,52 @@ class HomeView extends React.Component {
     
     render() {
         return (
-            <div id="home-view-container" className="ui container">
-            <DocumentTitle title='Home'>
-                <div className="ui middle aligned center aligned grid">
-                    <div className="column">
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                        <div onClick={() => this.props.dispatch(push('/map'))} className={"ui massive green button"}>
-                            <i className="fa fa-map-marker" aria-hidden="true"></i> Map
+            <div id="home-view-container" className="">
+            <DocumentTitle title='Home'></DocumentTitle>
+                <div className="ui vertical masthead center aligned segment">
+                    <div id="masthead-content" className="ui middle aligned center aligned grid">
+                        <div className="eight column wide">
+                            <div onClick={() => this.props.dispatch(push('/map'))} className={"ui massive green button"}>
+                                <i className="fa fa-map-marker" aria-hidden="true"></i> Map
+                            </div>
                         </div>
                     </div>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    i need copy for this page
                 </div>
-            </DocumentTitle>
-        </div>
+                <div className="ui vertical stripe segment">
+                    <div className="ui middle aligned stackable grid container">
+                        <div className="centered row">
+                            <h1 className="ui huge header">SEARCH. RENT. REVIEW.</h1>
+                        </div>
+                        <div id="three-benefits" className="row">
+                            <div className="third column">
+                                <img className="ui centered medium circular image" src="http://semantic-ui.com/images/wireframe/square-image.png"/>
+                                <h3 className="ui header centered">SEARCH FOR YOUR PERFECT HOME</h3>
+                            </div>
+                            <div className="third column">
+                                <img className="ui centered medium circular image" src="http://semantic-ui.com/images/wireframe/square-image.png"/>
+                                <h3 className="ui header centered">CONTACT OWNERS DIRECTLY</h3>
+                            </div>
+                            <div className="third column">
+                                <img className="ui centered medium circular image" src="http://semantic-ui.com/images/wireframe/square-image.png"/>
+                                <h3 className="ui header centered">REVIEW YOUR STAY</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="ui vertical stripe segment">
+                    <div className="ui middle aligned stackable grid container">
+                        <div className="centered row">
+                            <h1 className="ui huge header">STUDENT ACCOMODATION IN THE SPICE ISLAND</h1>
+                        </div>
+                        <div id="selling-points" className="centered row">
+                            <h3 className="ui header">The keys to renting in Grenada are now in your hands. No more ________, no more hard to find listings.</h3>
+                            <h3 className="ui header">With GNDAPTS, you now have access to the island’s comprehensive resource for available apartments and homes</h3>
+                            <h3 className="ui header">Whether you want beachside-living, affordable accomodation, or proximity to campus, find it on GNDAPTS</h3>
+                            <h3 className="ui header"> Help out your colleagues, classmates, and friends by reviewing your living experience!</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
