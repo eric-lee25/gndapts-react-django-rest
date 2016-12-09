0f1fae7536f77b0d9216e98a44565409e381c516
Manish Sinha <arithmetic@gmail.com>
2016-12-09 15:36:51 -0800

    turn off pagination

--- src/gndapts/settings/base.py
+++ src/gndapts/settings/base.py
@@ -95,9 +95,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
         'rest_framework.parsers.FormParser',
